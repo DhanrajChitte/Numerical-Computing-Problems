33feb486942be839f18d953146c4d6c459fae0f1
Dhanraj Sunil Chitte <156110967+DhanrajChitte@users.noreply.github.com>
2024-06-26 15:01:18 +0530

    Add files via upload

---
+++ BM.hpp
@@ -0,0 +1,19 @@
+#ifndef BM_HPP
+#define BM_HPP
+
+class BisectionMethod
+{
+    double a, b, delta; // private
+public:
+    BisectionMethod(double);
+    void NewtonRaphson();
+    void FixedPoint();
+
+    void find_interval();
+    void get_interval(double, double);
+    double f(double);
+    void bisection();
+    ~BisectionMethod();
+};
+
+#endif
